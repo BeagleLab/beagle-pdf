b06b237c6932d9f4c99ee8ee36f676eba67d9050
Richard Littauer <richard.littauer@gmail.com>
2015-02-25 18:16:53 -0800

    Added response catch if no doi, proper nesting for modules
    Linted according to standard, cleaned up package.json

--- index.js
+++ index.js
@@ -1,69 +1,64 @@
-var pdfjs = require('pdfjs-dist-for-node/build/pdf.combined.js');
-var altmetrics = require('beagle-altmetrics');
-var accum = require('accum-transform');
-var _ = require('lodash');
+var pdfjs = require('pdfjs-dist-for-node/build/pdf.combined.js')
+var altmetrics = require('beagle-altmetrics')
+var _ = require('lodash')
 var doiRegex = require('doi-regex')
 
-var altmetricsFromDoi = function(doi, cb) {
-  response = true;
-  return altmetrics.getDataFromDoi(doi, function(err, data){
+var altmetricsFromDoi = function (doi, cb) {
+  response = true
+  return altmetrics.getDataFromDoi(doi, function (err, data) {
     if (err !== null) {
-      console.error(err);
+      console.error(err)
       // What does process do?
-      process.exit(-1);
+      process.exit(-1)
     }
 
-    // console.log('Altmetrics Data:', data);
-    return cb(null, data);
-  });
+    return cb(null, data)
+  })
 }
 
-var response;
+var response
 
-var readPDF = function(documentObject, options, cb) {
-
-
+var readPDF = function (documentObject, options, cb) {
   if (!documentObject) {
-    console.log("No pdf");
-    throw new Error("no pdf");
+    console.log('No pdf')
+    throw new Error('no pdf');
   }
 
-  // console.log('Document object', documentObject);
+  pdfjs.getDocument(documentObject).then(function (pdf) {
+    // May not be useful at the moment.
+    pdf.getMetadata().then(function (data) {
+      console.log('Metadata:', data)
+    })
 
-  pdfjs.getDocument(documentObject).then(function(pdf) {
-
-    //May not be useful at the moment.
-    pdf.getMetadata().then(function(data){
-      console.log('Metadata:', data);
-    });
-
-    numPages = pdf.numPages;
+    var numPages = pdf.numPages
     // Define vars where they won't be redefined in each loop in if statements
-    var pageInfo, doi, match;
+    var doi, match
 
-    for (i = 1; i <= numPages; i++) {
-      pdf.getPage(i).then(function(page) {
-        page.getTextContent().then(function(textContent) {
-          _.each(textContent.items, function(item){
+    for (var i = 1; i <= numPages; i++) {
+      pdf.getPage(i).then(function (page) {
+        page.getTextContent().then(function (textContent) {
+          _.each(textContent.items, function (item) {
             // TODO match[2] tracks .t001, .g001, etc. Capture these, they may be relevant
             // to research.
-            if (!!doiRegex.groups(item.str))
+            if (doiRegex.groups(item.str))
               match = doiRegex.groups(item.str)
 
             if (match && !response) {
               if (!doi) {
                 // Only call once, for now. TODO Multiple DOIs
-                doi = match[1];
-                if (options.altmetrics) altmetricsFromDoi(doi, cb);
-
+                doi = match[1]
+                var response = true
+                if (options.modules.altmetrics) altmetricsFromDoi(doi, cb)
               }
             }
-          });
-        });
-      });
+          })
+        })
+      })
     }
-  });
-};
 
-exports.pdfjs = pdfjs;
-exports.readPDF = readPDF;
+    if (!response) { return cb(null, null) }
+  })
+}
+
+exports.pdfjs = pdfjs
+exports.readPDF = readPDF
