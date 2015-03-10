ab5edf0b8519efe03e43eb3973d9946fa439520a
Richard Littauer <richard.littauer@gmail.com>
2015-03-09 19:52:12 -0700

    Added highlight and get coords functions

--- index.js
+++ index.js
@@ -71,7 +71,37 @@ var readPDFText = function (documentObject, options, cb) {
   })
 }
 
+var getHightlightCoords = function () {
+  var pageIndex = window.PDFViewerApplication.pdfViewer.currentPageNumber - 1
+  var page = window.PDFViewerApplication.pdfViewer.pages[pageIndex]
+  var pageRect = page.canvas.getClientRects()[0]
+  var selectionRects = window.getSelection().getRangeAt(0).getClientRects()
+  var viewport = page.viewport
+  var selected = _.map(selectionRects, function (r) {
+    return viewport.convertToPdfPoint(r.left - pageRect.left, r.top - pageRect.top).concat(
+       viewport.convertToPdfPoint(r.right - pageRect.left, r.bottom - pageRect.top))
+  })
+  return {page: pageIndex, coords: selected}
+}
+
+var showHighlight = function (selected) {
+  var pageIndex = selected.page
+  var page = window.PDFViewerApplication.pdfViewer.pages[pageIndex]
+  var pageElement = page.canvas.parentElement
+  var viewport = page.viewport
+  selected.coords.forEach(function (rect) {
+    var bounds = viewport.convertToViewportRectangle(rect)
+    var el = document.createElement('div')
+    el.setAttribute('style', 'position: absolute; background-color: rgba(238, 170, 0, .2);' +
+      'left:' + Math.min(bounds[0], bounds[2]) + 'px; top:' + Math.min(bounds[1], bounds[3]) + 'px;' +
+      'width:' + Math.abs(bounds[0] - bounds[2]) + 'px; height:' + Math.abs(bounds[1] - bounds[3]) + 'px;')
+    pageElement.appendChild(el)
+  })
+}
+
 exports.pdfjs = pdfjs
 exports.getFingerprint = getFingerprint
 exports.getMetadata = getMetadata
 exports.readPDFText = readPDFText
+exports.getHightlightCoords = getHightlightCoords
+exports.showHighlight = showHighlight
